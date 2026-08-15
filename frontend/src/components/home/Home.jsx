c000588c352902d596eaab0fa44a9a593b07545c
sheeshagar-iqbal <iqbalkhan234786@gmail.com>
2026-08-15 12:08:24 +0530

    handle ui home page

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import {Usercontext} from '../../context/Dataprovide'
-import { useContext } from 'react'
-import Bannner from '../banner/Bannner'
-import Category from './Category'
-import { Grid } from '@mui/material'
-import BlogAll from '../show/BlogAll'
+import React from "react";
+import { Usercontext } from "../../context/Dataprovide";
+import { useContext } from "react";
+import Bannner from "../banner/Bannner";
+import Category from "./Category";
+import { Grid ,Typography } from "@mui/material";
+import BlogAll from "../show/BlogAll";
 
 const Home = () => {
-    const {account}=useContext(Usercontext)
+  const { account } = useContext(Usercontext);
   return (
     <>
-    <h2>{account.email}</h2>
-    <Bannner/>
+      <h2>{account.email}</h2>
+      <Bannner />
 
-   <Grid container>
-     <Grid container lg={2} sm={2} xs={12}>
-    <Category/>
-    </Grid>
-    <Grid container  lg={10} sm={10} xs={12}>
-      {/* <div style={{display:'flex', flexDirection:'column',} }> */}
-        <h1 style={{margin:'20px 30px'}}>New Blog</h1>
-      <BlogAll/>
-      {/* </div> */}
-    </Grid>
-   </Grid>
+    <Grid container spacing={2}>
+
+  <Grid size={{ xs: 12, sm: 3, md: 3, lg: 2 }}>
+    <Category />
+  </Grid>
+
+  <Grid size={{ xs: 12, sm: 9, md: 9, lg: 10 }} spacing={5}>
+    <Typography variant="h4" sx={{ m: 3 }}>
+      New Blog
+    </Typography>
+
+    <BlogAll />
+  </Grid>
+
+</Grid>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- frontend/src/components/show/BlogAll.jsx
+++ frontend/src/components/show/BlogAll.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import BlogCard from "./BlogCard";
 import axios from "axios";
 
@@ -15,19 +15,23 @@ const BlogAll = () => {
   return (
     <Box
       sx={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: 3,
-        justifyContent: "center",
+        // display: "flex",
+        // flexWrap: "wrap",
+        // gap: 3,
+        // justifyContent: "center",
         margin: "30px",
       }}
     >
+      <Grid container spacing={2}>
       {blogs.map((blog, index) => (
+        <Grid  size={{lg:4, md:6, sm:6, xs:12}} key={blog._id}>
         <BlogCard
           key={index}
           blog={blog}
         />
+        </Grid>
       ))}
+      </Grid>
     </Box>
   );
 };
